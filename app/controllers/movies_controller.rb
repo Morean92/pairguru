21778cdff4e267ce375a65127b79ec940bbb9ce4
Morean92 <pawel.dukarski@live.com>
2017-02-13 11:16:03 +0100

    Using decorator to show poster, overview and rating + exposing one movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,20 +2,7 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, only: [:send_info]
 
   expose_decorated(:movies) { Movie.includes(:genre).paginate(:page => params[:page], :per_page => 20) }
-  expose(:movie)
-
-  def show
-    if mov = Tmdb::Search.movie(movie.title)
-      poster_path = mov.results.first.poster_path
-      @poster_image = "https://image.tmdb.org/t/p/w640/#{poster_path}"
-      @overview = mov.results.first.overview
-      @rating = mov.results.first.vote_average
-    else
-      @rating = 'no data'
-      @poster_image = ''
-      @overview = 'no data'
-    end
-  end
+  expose_decorated(:movie)
 
   def send_info
     MovieInfoMailer.send_info(current_user, movie).deliver_now
--- app/decorators/movie_decorator.rb
+++ app/decorators/movie_decorator.rb
@@ -8,5 +8,22 @@ class MovieDecorator < Draper::Decorator
   def cover
     'http://lorempixel.com/100/150/' + %W(abstract nightlife transport).sample + '?a=' + SecureRandom.uuid
   end
+
+  def poster_image
+    config = Tmdb::Configuration.get
+    base_url = config.images.secure_base_url
+    size = config.images.poster_sizes[4]
+    image_path = Tmdb::Search.movie(movie.title).results.first.poster_path
+
+    base_url + size + image_path
+  end
+
+  def overview
+    Tmdb::Search.movie(movie.title).results.first.overview
+  end
+
+  def rating
+    Tmdb::Search.movie(movie.title).results.first.vote_average
+  end
 end
  
